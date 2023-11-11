58a5123772edd58e1cb2837d6db7b834377e21ec
skyglass <skyglass2001@gmail.com>
2023-11-11 22:25:00 +0100

    prepare for deployment

---
+++ module-app/c4-01-ebs-csi-datasources.tf
@@ -0,0 +1,10 @@
+# Datasource: EBS CSI IAM Policy get from EBS GIT Repo (latest)
+data "http" "ebs_csi_iam_policy" {
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-ebs-csi-driver/master/docs/example-iam-policy.json"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "application/json"
+  }
+}
+
